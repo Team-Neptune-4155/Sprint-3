18160e1adb208fe63cdba34d0f478a5665739452
Yvette Soto <yvettesoto25@gmail.com>
2025-04-25 16:02:08 -0400

    Added buttons to index.html to reach contact/about.html

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -4,7 +4,7 @@ from . import views
 # url configuration module
 # Mapping URLs
 urlpatterns = [
-    path('', views.main_page),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('', views.main_page, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
